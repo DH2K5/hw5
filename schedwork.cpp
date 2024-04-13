104a2354f419ae4ecd839fece1706db6a43f6e6a
Akul Jindal <jindalakul@gmail.com>
2024-04-12 19:33:58 -0700

    All tests successful 1

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduler (
+    const AvailabilityMatrix&,
+    const size_t,
+    const size_t,
+    DailySchedule&,
+    map<Worker_T, size_t>,
+    size_t,
+    size_t
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +45,56 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
-
-
-
-
+    if(avail[0].size() < dailyNeed){
+        return false;
+    }
+    map<Worker_T, size_t> days;
+    for(size_t i = 0; i < avail[0].size(); i++){
+        days.insert(pair<Worker_T, size_t> (i, 0));
+    }
+    for(size_t i = 0; i < avail.size(); i++){
+        vector<Worker_T> day;
+        sched.push_back(day);
+        for (size_t j = 0; j < dailyNeed; j++)
+        {
+            sched[i].push_back(0);
+        }
+    }
+    return scheduler(avail, dailyNeed, maxShifts, sched, days, 0, 0);
 }
 
+bool scheduler (
+    const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    map<Worker_T, size_t> days,
+    size_t currentDay,
+    size_t currentshift
+)
+{
+    if(currentDay >= avail.size()){
+        return true;
+    }
+    for(int i = 0; i < ((int) avail[currentDay].size()); i++){
+        sched[currentDay][currentshift] = i;
+        days[i]++;
+        currentshift++;
+        if((avail[currentDay][i] == 1) && (days[i] <= maxShifts)){
+            if(currentshift == dailyNeed){
+                if(scheduler(avail, dailyNeed, maxShifts, sched, days, currentDay+1, 0)){
+                    return true;
+                }
+            }
+            else{
+                if(scheduler(avail, dailyNeed, maxShifts, sched, days, currentDay, currentshift)){
+                    return true;
+                }
+            }
+        }
+        currentshift--;
+        days[i]--;
+        sched[currentDay][currentshift] = 0;
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,8 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-void wordhelper(set<string>&, string, const set<string>&);
-void fixfloat(set<string>&, string, string);
+int unfilledCount(const string&);
+void wordhelper(set<string>&, string, string, const set<string>&, unsigned int);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,76 +23,48 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    string floatingg = floating;
     set<string> list;
     string inn = in;
-    string floatt = floating;
 
-    wordhelper(list, inn, dict);
-
-    fixfloat(list, inn, floatt);
-
+    wordhelper(list, inn, floatingg, dict, 0);
     return list;
 }
 
+
 // Define any helper functions here
-void wordhelper(set<string>& list, string in, const set<string>& dict){
-    for(int i = 0; i < ((int) in.length()); i++){
-        if(in[i] == '-'){
-            for(int j = 97; j < 123; j++){
-                in[i] = j;
-                wordhelper(list, in, dict);
-            }
-            in[i] = '-';
-        }
+int unfilledCount(const string& currstr){
+  int count = 0;
+  for (int k = 0; k < ((int) currstr.length()); k++){
+    if(currstr[k] == '-'){
+        count++;
     }
-    if((dict.find(in)) == dict.end()){
-        return;
-    }
-    list.insert(in);
+  }
+  return count;
 }
 
-void fixfloat(set<string>& list, string in, string floating){
-    set<string>::iterator i = list.begin();
-    set<string>::iterator j = list.begin();
-    string inn;
-    while(i != list.end()){
-        bool flag = true;
-        inn = *i;
-        for(int k = 0; k < ((int) in.length()); k++){
-            if(in[k] == '-'){}
-            else if(inn[k] != in[k]){
-                j = i;
-                ++i;
-                list.erase(j);
-                flag = false;
-                break;
-            }
-            else{
-                inn[k] = '-';
-            }
+void wordhelper(set<string>& list, string in, string floating, const set<string>& dict, unsigned int curr)
+{
+    if(curr == in.size()){
+        if((dict.find(in) != dict.end()) && (floating.length() == 0)){ 
+            list.insert(in);
         }
-        if(flag){
-            for(int k = 0; k < ((int) floating.length()); k++){
-                bool match = true;
-                for(int l = 0; l < ((int) inn.length()); l++){
-                    if(inn[l] == floating[k]){
-                        match = false;
-                        inn[l] = '-';
-                        break;
-                    }
-                }
-                if(match){
-                    j = i;
-                    ++i;
-                    list.erase(j);
-                    flag = false;
-                    break;
-                }
+        return;
+    }
+    else if(in[curr] != '-'){
+        wordhelper(list, in, floating, dict, curr + 1);
+    }
+    else{
+        for(int j = 0; j < ((int) floating.length()); j++){
+            in[curr] = floating[j];
+            string substr = (floating.substr(0,j) + floating.substr(j+1));
+            wordhelper(list, in, substr, dict, curr + 1);
+        }
+        if(unfilledCount(in) >= ((int) floating.length())){
+            for(char i = 97; i < 123; i++){
+                in[curr] = i;
+                wordhelper(list, in, floating, dict, curr + 1);
             }
-            if(flag){
-                ++i;
-            }
-
         }
     }
 }
